9afe451f11e3669bb596ec2787c696440c0dd032
chirawat <chirawat.not@gmail.com>
2017-02-22 12:50:54 +0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyCFM-W9UXcOz2QgUokYkDNixvqOgpPbbpw",
+  authDomain: "amber-inferno-700.firebaseapp.com",
+  databaseURL: "https://amber-inferno-700.firebaseio.com",
+  storageBucket: "amber-inferno-700.appspot.com",
+  messagingSenderId: "880766025543"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +22,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
